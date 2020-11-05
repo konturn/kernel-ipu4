zhonghuaY/acrn-kernel

media: intel-ipu4: [VIRT] Release ISYS buffer during stream node close.
During AaaG reboot testing, sometimes the stream off did not
get called but only stream node close. This caused
issue to subsequent run where SOS and UOS get out
of sync.

Change-Id: Id85bf2261d3ffcf2fd928b4bc1950719b91ddf9a
Tracked-On: OAM-72430
Signed-off-by: Ong Hock Yu <ong.hock.yu@intel.com>

drivers/media/pci/intel/ici/ici-isys-frame-buf.c
@@ -740,11 +740,12 @@ void ici_isys_frame_buf_stream_cancel(struct
 {
 	struct ici_isys_frame_buf_list *buf_list = &as->buf_list;
 	struct ici_frame_buf_wrapper *buf;
-	struct ici_frame_buf_wrapper *next_buf;
 	unsigned long flags = 0;
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->getbuf_list, node) {
+	while(!list_empty(&buf_list->getbuf_list)) {
 		spin_lock_irqsave(&buf_list->lock, flags);
+		buf = list_entry(buf_list->getbuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->lock, flags);
 		if (as->strm_dev.virt_dev_id < 0)
@@ -753,8 +754,10 @@ void ici_isys_frame_buf_stream_cancel(struct
 			unmap_buf_virt(buf);
 	}
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->putbuf_list, node) {
+	while(!list_empty(&buf_list->putbuf_list)) {
 		spin_lock_irqsave(&buf_list->lock, flags);
+		buf = list_entry(buf_list->putbuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->lock, flags);
 		if (as->strm_dev.virt_dev_id < 0)
@@ -763,9 +766,10 @@ void ici_isys_frame_buf_stream_cancel(struct
 			unmap_buf_virt(buf);
 	}
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->interlacebuf_list,
-								node) {
+	while(!list_empty(&buf_list->interlacebuf_list)) {
 		spin_lock_irqsave(&buf_list->short_packet_queue_lock, flags);
+		buf = list_entry(buf_list->interlacebuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->short_packet_queue_lock, flags);
 		unmap_buf(buf);

konturn/kernel-ipu4

Change-Id: Ib79ce44e714d34309c181d9082db7a1d7e70536e

drivers/media/pci/intel/ici/ici-isys-frame-buf.c
@@ -740,11 +740,12 @@ void ici_isys_frame_buf_stream_cancel(struct
 {
 	struct ici_isys_frame_buf_list *buf_list = &as->buf_list;
 	struct ici_frame_buf_wrapper *buf;
-	struct ici_frame_buf_wrapper *next_buf;
 	unsigned long flags = 0;
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->getbuf_list, node) {
+	while (!list_empty(&buf_list->getbuf_list)) {
 		spin_lock_irqsave(&buf_list->lock, flags);
+		buf = list_entry(buf_list->getbuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->lock, flags);
 		if (as->strm_dev.virt_dev_id < 0)
@@ -753,8 +754,10 @@ void ici_isys_frame_buf_stream_cancel(struct
 			unmap_buf_virt(buf);
 	}
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->putbuf_list, node) {
+	while (!list_empty(&buf_list->putbuf_list)) {
 		spin_lock_irqsave(&buf_list->lock, flags);
+		buf = list_entry(buf_list->putbuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->lock, flags);
 		if (as->strm_dev.virt_dev_id < 0)
@@ -763,9 +766,10 @@ void ici_isys_frame_buf_stream_cancel(struct
 			unmap_buf_virt(buf);
 	}
 
-	list_for_each_entry_safe(buf, next_buf, &buf_list->interlacebuf_list,
-								node) {
+	while (!list_empty(&buf_list->interlacebuf_list)) {
 		spin_lock_irqsave(&buf_list->short_packet_queue_lock, flags);
+		buf = list_entry(buf_list->interlacebuf_list.next,
+			struct ici_frame_buf_wrapper, node);
 		list_del(&buf->node);
 		spin_unlock_irqrestore(&buf_list->short_packet_queue_lock, flags);
 		unmap_buf(buf);